7022fcd94fdbdc717b90389461670a1e66154bfa
KRISHNA-165 <gundabalakrishna2005@gmail.com>
2026-02-19 10:20:45 +0530

    UC5: lifo and fifo based palindrome check

---
+++ src/UseCase6PalindromeCheckerApp.java
@@ -0,0 +1,44 @@
+import java.util.Queue;
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class UseCase6PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Original string
+        String word = "level";
+
+        // Create Queue and Stack
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        // Enqueue and push characters
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            queue.add(ch);   // FIFO
+            stack.push(ch);  // LIFO
+        }
+
+        // Compare dequeue and pop
+        boolean isPalindrome = true;
+
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        System.out.println("Palindrome Checker App");
+        System.out.println("UC6: Queue + Stack Based Palindrome Check");
+
+        if (isPalindrome) {
+            System.out.println("The word \"" + word + "\" is a palindrome.");
+        } else {
+            System.out.println("The word \"" + word + "\" is NOT a palindrome.");
+        }
+    }
+}
+
