99c8a864d162236d17235399eb1476485024ae0d
KRISHNA-165 <gundabalakrishna2005@gmail.com>
2026-02-19 09:41:42 +0530

    UC2:Hardcoded palindrome

---
+++ src/UseCase2PalindromeCheckerApp.java
@@ -0,0 +1,23 @@
+public class UseCase2PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        // Hardcoded string
+        String word = "madam";
+
+        // Reverse the string
+        String reversed = new StringBuilder(word).reverse().toString();
+
+        // Check palindrome using if-else
+        if (word.equals(reversed)) {
+            System.out.println("Palindrome Checker App");
+            System.out.println("UC2: Print a Hardcoded Palindrome Result");
+            System.out.println("The word \"" + word + "\" is a palindrome.");
+        } else {
+            System.out.println("Palindrome Checker App");
+            System.out.println("UC2: Print a Hardcoded Palindrome Result");
+            System.out.println("The word \"" + word + "\" is NOT a palindrome.");
+        }
+    }
+}
+
